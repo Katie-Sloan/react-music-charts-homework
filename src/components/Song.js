8385d0c99425c4503bcacadaf40ee174ff9fae30
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-26 16:41:07 +0000

    initial commit

---
+++ src/components/Song.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Song = () => {
+    return(
+        null
+    )
+}
+
+export default Song;
---
+++ src/containers/SongContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SongList from "../components/SongList";
+import Song from "../components/Song";
+
+const SongContainer = () => {
+    return(
+        null
+    )
+}
+
+export default SongContainer;
